3fa7327a53404d9d1fa26aad559da183fa94f5fa
MelSchiffner <156306012+MelSchiffner@users.noreply.github.com>
2024-03-06 20:10:09 +0100

    installing and setting up TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    animatedSettings: {
+      animatedSpeed: 1000,
+      heartBeatSpeed: 500,
+      hingeSpeed: 2000,
+      bounceInSpeed: 750,
+      bounceOutSpeed: 750,
+      animationDelaySpeed: 500,
+      classes: ['bounce', 'heartBeat']
+  },
+    extend: {},
+  },
+  plugins: [require('tailwindcss-animatecss')],
+}
