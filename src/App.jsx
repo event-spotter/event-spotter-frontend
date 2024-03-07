2b3b79f8ebf1f8dd1ef0035f4cf653b1bc7a39f1
MelSchiffner <156306012+MelSchiffner@users.noreply.github.com>
2024-03-07 17:40:35 +0100

    solving merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,16 @@ import "./App.css";
 import {BrowserRouter as Router, Routes, Route, Navigate,} from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
-<<<<<<< HEAD
-=======
-import NavBar from "./components/NavBar";
-
->>>>>>> fe90d8f (add navbar)
 import Dashboard from "./pages/Dashboard";
 import HomePage from "./pages/HomePage";
+import NavBar from "./components/Navbar"
+
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-=======
-      <div className="text-blue-700 flex items-center justify-center">
-        Event Spotter
-      </div>
-      <NavBar />
-
->>>>>>> fe90d8f (add navbar)
+   <NavBar />
+   
       <Routes>
         <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="/dashboard" element={<Dashboard />} />
