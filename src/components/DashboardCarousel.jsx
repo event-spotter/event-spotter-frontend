2526dce79262c6a9cfce89a8ec39202ae8cc8783
MelSchiffner <156306012+MelSchiffner@users.noreply.github.com>
2024-03-07 22:01:24 +0100

    adjusted some tailwindcss on dashboardcarousel

--- src/components/DashboardCarousel.jsx
+++ src/components/DashboardCarousel.jsx
@@ -12,14 +12,16 @@ import {
 function DashboardCarousel({events}) {
   return (
     <div className="flex items-center justify-center">
-    <Carousel className="w-11/12 max-w-xl h-11/12">
+    <Carousel className="w-11/12 max-w-4xl h-11/12 mt-8 mb-8">
       <CarouselContent className="-ml-1 md:-ml-4">
         {events.slice(0, 5).map((event, index) => (
           <CarouselItem key={event.id || index} className="pl-2 md:basis-1/2 lg:basis-2/4">
             <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-2xl font-semibold">{event.title}</span>
+              <Card className="h-80 w-80" >
+                <CardContent className="flex flex-col gap-8 items-center justify-center">
+                  <span className="text-xl font-semibold">{event.title}</span>
+                  <span className="text-lg font-semibold">{event.description}</span>
+                  <span className="text-md font-semibold">{event.location}</span>
                 </CardContent>
               </Card>
             </div>
