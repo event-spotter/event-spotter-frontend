c2421ca960b053d8db1e7a3fbe5fa682f7dc9e19
Senay Asikoglu <senayasikoglu@gmail.com>
2024-03-13 13:17:46 +0100

    add css to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,35 +14,43 @@ return (
     <div className="relative inset-0 flex items-center justify-center">
       <img
         src={ CreateEvent}
-        className="object-cover w-full h-52"
+        className="object-cover w-full h-72" 
         alt="Background"
       />
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white">
-        <Link to={"/events"} className="font-extrabold  font-sans shadow-md text-2xl mb-8 text-yellow-300 hover:text-sky-900 ">Explore Events</Link>
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white hover:bg-transparent/30">
+        <Link to={"/events"} 
+        className="font-extrabold font-sans text-2xl mb-8 text-yellow-300 
+        hover:text-white   hover:border-2  hover:border-white hover:border py-5 px-8
+        rounded hover:rounded-sm ">Explore Events</Link>
       </div>
       </div>
 
-      <div className="relative inset-0 flex items-center justify-center">
+      <div className="relative  inset-0 flex items-center justify-center">
       <img
         src={HandsUp}
-        className="object-cover w-full h-52"
+        className="object-cover w-full h-60"
         alt="Background"
       />
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white">
-        <Link to={"/addEvent"} className="font-extrabold font-sans text-2xl mb-8 text-yellow-300 hover:text-red-800">Create Event</Link>
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white hover:bg-transparent/30">
+        <Link to={"/addEvent"} 
+        className="font-extrabold font-sans text-2xl mb-8 text-yellow-300 
+        hover:text-white   hover:border-2  hover:border-white hover:border py-5 px-8
+        rounded hover:rounded-sm ">Create Event</Link>
       </div>
       </div>
 
       <div className="relative inset-0 flex items-center justify-center">
       <img
         src={ExploreArtists}
-        className="object-cover w-full h-52"
+        className="object-cover w-full h-60"
         style={{ objectPosition: '50% 10%' }}
         alt="Background"
       />
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white">
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white hover:bg-transparent/30">
       
-        <Link to={"/artists"} className="font-extrabold  font-sans shadow-md text-2xl mb-8 text-yellow-300 hover:text-red-800">Explore Artists</Link>
+        <Link to={"/artists"} className="font-extrabold font-sans text-2xl mb-8 text-yellow-300 
+        hover:text-white   hover:border-2  hover:border-white hover:bg-transparent/40 hover:border py-5 px-8
+        rounded hover:rounded-sm ">Explore Artists</Link>
       
       </div>
       </div>
